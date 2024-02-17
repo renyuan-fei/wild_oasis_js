e6949f3bdf82d61b8c307b8bce52c40ad145b618
Fry <renyuan.fei.1020@gmail.com>
2024-02-18 03:12:20 +1030

    Remove hardcoded login credentials from LoginForm
    LoginForm.jsx no longer contains hardcoded email and password, instead it initializes with blank strings. This fix is essential for improved security and proper implementation of the login function.

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import {useLogin} from './useLogin.js';
 import SpinnerMini from '../../ui/SpinnerMini.jsx';
 
 function LoginForm() {
-  const [email, setEmail] = useState("renyuan.fei.1020@gmail.com");
-  const [password, setPassword] = useState("Fry236278128");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const {login, isLoading} = useLogin();
   
